f1895dcaf64e953482d30e14c2430763acf9effd
Daniil Yakovlev <ddyakovlev@yandex.ru>
2019-07-04 17:59:22 +0700

    add 8.cc

---
+++ 8.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+
+static const int a = 10, b = 2, c = 2, d = 10;
+
+static const auto f1 = [](double x, double y, double t) {
+    return a * x - b * x * y;
+};
+
+static const auto f2 = [](double x, double y, double t) {
+    return c * x * y - d * y;
+};
+
+std::tuple<std::vector<double>, std::vector<double>, std::vector<double>>
+RungeKuttaMethods(int N, double alpha, std::tuple<double, double> InitPoint, std::pair<double, double> bounds)
+{
+    std::vector<double> x, y, t;
+    x.push_back(std::get<0>(InitPoint));
+    y.push_back(std::get<1>(InitPoint));
+    t.push_back(bounds.first);
+    double h = (bounds.second - bounds.first) / N;
+    for (int i = 0; i < N; ++i) 
+    {
+        x.push_back(x[i] + h * ((1 - alpha) * f1(x[i], y[i], y[i]) + alpha * f1(x[i] + h * 0.5 / alpha * f1(x[i], y[i], t[i]),
+                                                                                y[i] + h * 0.5 / alpha * f2(x[i], y[i], t[i]),
+                                                                                t[i] + h * 0.5 / alpha)));
+        y.push_back(y[i] + h * ((1 - alpha) * f2(x[i], y[i], y[i]) + alpha * f2(x[i] + h * 0.5 / alpha * f1(x[i], y[i], t[i]),
+                                                                                y[i] + h * 0.5 / alpha * f2(x[i], y[i], t[i]),
+                                                                                t[i] + h * 0.5 / alpha)));
+        t.push_back(t[i] + h);
+    }
+    return {x, y, t};
+}
+
+static const double t1 = 0, t2 = 5;
+static const double predator = 6, prey = 6;
+static const double alpha = 0.75;
+static const int iter = 100;
+
+int main() {
+    auto solution = RungeKuttaMethods(iter, alpha, {predator, prey}, {t1, t2});
+    for (int i = 0; i < iter; ++i)
+    std::cout << std::get<0>(solution)[i] << '\t' << std::get<1>(solution)[i]  << '\t' << std::get<2>(solution)[i] << std::endl;
+    return 0; 
+}
